9dd6fcd6320520e7e130cf8626c77e63bb268aa2
albertec1 <45201952+albertec1@users.noreply.github.com>
2021-09-22 09:34:28 +0200

    First Commit
    Basic Project with the requiered libraries

---
+++ Clever-Engine/Clever-Engine/Source Code/OpenGl.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#ifndef __OPENGL_H__
+#define __OPENGL_H__
+
+#define WIN32_MEAN_AND_LEAN
+#include <Windows.h>
+#include "glew/include/GL/glew.h" // extension lib
+#include "SDL/include/SDL_opengl.h"
+#include <gl/GL.h>
+#include <gl/GLU.h>
+
+#endif
---
+++ Clever-Engine/README.md
@@ -0,0 +1,2 @@
+# Summer-Engine
+Basic OpenGL Game Engine. Made for the Engines Subject of the Videogame Design and Development degree at UPC (Universitat Politècnica de Catalunya - CITM)
