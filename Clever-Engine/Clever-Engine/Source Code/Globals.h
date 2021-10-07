c7c8746c61b8b70236dfa54f3049f28ee501f696
albertec1 <45201952+albertec1@users.noreply.github.com>
2021-10-07 15:19:53 +0200

    Prograss with the configuration window, FPS cap addad and working

--- Clever-Engine/Clever-Engine/Source Code/Globals.h
+++ Clever-Engine/Clever-Engine/Source Code/Globals.h
@@ -9,7 +9,7 @@
 
 #define LOG(format, ...) log(__FILE__, __LINE__, format, __VA_ARGS__);
 
-const char* log(const char file[], int line, const char* format, ...);
+void log(const char file[], int line, const char* format, ...);
 
 #define CAP(n) ((n <= 0.0f) ? n=0.0f : (n >= 1.0f) ? n=1.0f : n=n)
 
--- Clever-Engine/Clever-Engine/Source Code/ModuleUI.cpp
+++ Clever-Engine/Clever-Engine/Source Code/ModuleUI.cpp
@@ -11,10 +11,9 @@
 #include "Dependencies/ImGui/imgui_impl_opengl3.h" 
 
 
-ModuleUI::ModuleUI(Application* app, bool start_enabled) : Module(app, start_enabled)
-{
-
-}
+ModuleUI::ModuleUI(Application* app, bool start_enabled) : Module(app, start_enabled),
+fps_log(LOG_LENGTH), ms_log(LOG_LENGTH)
+{}
 
 // Destructor
 ModuleUI::~ModuleUI()
@@ -56,7 +55,9 @@ bool ModuleUI::Start()
 
 update_status ModuleUI::PreUpdate(float dt)
 {
-    //Begin new ImGui Frame
+    //Begin new ImGui Frame	
+    ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+
     ImGui_ImplOpenGL3_NewFrame();
     ImGui_ImplSDL2_NewFrame(App->window->window);
     ImGui::NewFrame();
@@ -65,6 +66,12 @@ update_status ModuleUI::PreUpdate(float dt)
     ImGuiIO& io = ImGui::GetIO();
     usingKeyboard = io.WantCaptureKeyboard;
     usingMouse = io.WantCaptureMouse;
+
+    //check for fps limit changes and update the limit in case it has changed
+    if (changeFPSlimit)
+    {
+        App->SetFRLimit(max_fps);
+    }
 
     return UPDATE_CONTINUE;
 }
@@ -75,82 +82,105 @@ update_status ModuleUI::Update(float dt)
     {
         App->input->ExitApp();
     }
+    // We are using the ImGuiWindowFlags_NoDocking flag to make the parent window not dockable into,
+    // because it would be confusing to have two docking targets within each others.
+    ImGuiWindowFlags window_flags = ImGuiWindowFlags_MenuBar | ImGuiWindowFlags_NoDocking;
 
-	if (ImGui::BeginMainMenuBar())
-	{
-		if (ImGui::BeginMenu("File"))
-		{
-            if (ImGui::MenuItem("New Scene"))
-            {
-                // New Scene code
-            }
-            if (ImGui::MenuItem("Open Scene"))
-            {
-                // Open Scene code
-            }
-            if (ImGui::MenuItem("Save Scene"))
-            {
-                // Save Scene code
-            }
-            if (ImGui::MenuItem("Save Scene as"))
-            {
-                // New Scene code
-            }
-            ImGui::Separator();
-            if (ImGui::MenuItem("Exit"))
-            {
-                App->input->ExitApp();
-            }
-			ImGui::EndMenu();
-		}
+    ImGuiViewport* viewport = ImGui::GetMainViewport();
+    ImGui::SetNextWindowPos(viewport->GetWorkPos());
+    ImGui::SetNextWindowSize(viewport->GetWorkSize());
+    ImGui::SetNextWindowViewport(viewport->ID);
+    ImGui::PushStyleVar(ImGuiStyleVar_WindowRounding, 0.0f);
+    ImGui::PushStyleVar(ImGuiStyleVar_WindowBorderSize, 0.0f);
+    window_flags |= ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove;
+    window_flags |= ImGuiWindowFlags_NoBringToFrontOnFocus | ImGuiWindowFlags_NoNavFocus |ImGuiWindowFlags_NoBackground;
+    ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
 
-        if (ImGui::BeginMenu("Create"))
+    if (ImGui::Begin("Dockspace", &activeDockingSpace, window_flags))
+    {
+        ImGui::PopStyleVar(3);
+        ImGuiID dockspace_id = ImGui::GetID("MyDockSpace");
+        ImGui::DockSpace(dockspace_id, ImVec2(0.0f, 0.0f), ImGuiDockNodeFlags_PassthruCentralNode);
+        
+        if (ImGui::BeginMainMenuBar())
         {
-            // Insert options here
+            if (ImGui::BeginMenu("File"))
+            {
+                if (ImGui::MenuItem("New Scene"))
+                {
+                    // New Scene code
+                }
+                if (ImGui::MenuItem("Open Scene"))
+                {
+                    // Open Scene code
+                }
+                if (ImGui::MenuItem("Save Scene"))
+                {
+                    // Save Scene code
+                }
+                if (ImGui::MenuItem("Save Scene as"))
+                {
+                    // New Scene code
+                }
+                ImGui::Separator();
+                if (ImGui::MenuItem("Exit"))
+                {
+                    App->input->ExitApp();
+                }
+                ImGui::EndMenu();
+            }
 
-            ImGui::EndMenu();
+            if (ImGui::BeginMenu("Create"))
+            {
+                // Insert options here
+
+                ImGui::EndMenu();
+            }
+
+            if (ImGui::BeginMenu("Window"))
+            {
+                ImGui::Checkbox("Console", &activeConsole);
+                ImGui::Checkbox("Configuration", &activeConfiguration);
+
+                ImGui::EndMenu();
+            }
+
+            if (ImGui::BeginMenu("Help"))
+            {
+                // Insert options here
+
+                ImGui::EndMenu();
+            }
+
+            if (ImGui::BeginMenu("Development"))
+            {
+                if (ImGui::MenuItem("ImGui Github"))
+                    App->RequestBrowser("https://github.com/ocornut/imgui");
+
+                if (ImGui::MenuItem("MathGeoLib Github"))
+                    App->RequestBrowser("https://github.com/juj/MathGeoLib");
+
+                ImGui::EndMenu();
+            }
+            if (ImGui::BeginMenu("About"))
+            {
+                if (ImGui::MenuItem("Documentation"))
+                    App->RequestBrowser("https://github.com/WittIsHere/Clever-Engine/wiki");
+
+                if (ImGui::MenuItem("Download latest"))
+                    App->RequestBrowser("https://github.com/WittIsHere/Clever-Engine/releases");
+
+                if (ImGui::MenuItem("Report a bug"))
+                    App->RequestBrowser("https://github.com/WittIsHere/Clever-Engine/issues");
+                ImGui::EndMenu();
+            }
+
+            ImGui::EndMenuBar();
+            ImGui::End();
         }
 
-        if (ImGui::BeginMenu("Window"))
-        {
-            ImGui::Checkbox("Console", &activeConsole);
-
-            ImGui::EndMenu();
-        }
-
-        if (ImGui::BeginMenu("Help"))
-        {
-            // Insert options here
-
-            ImGui::EndMenu();
-        }
-
-        if (ImGui::BeginMenu("Development"))
-        {
-            if (ImGui::MenuItem("ImGui Github"))
-                App->RequestBrowser("https://github.com/ocornut/imgui");
-           
-            if (ImGui::MenuItem("MathGeoLib Github"))
-                App->RequestBrowser("https://github.com/juj/MathGeoLib");
-
-            ImGui::EndMenu();
-        }
-        if (ImGui::BeginMenu("About"))
-        {
-            if (ImGui::MenuItem("Documentation"))
-                App->RequestBrowser("https://github.com/WittIsHere/Clever-Engine/wiki");
-
-            if (ImGui::MenuItem("Download latest"))
-                App->RequestBrowser("https://github.com/WittIsHere/Clever-Engine/releases");
-
-            if (ImGui::MenuItem("Report a bug"))
-                App->RequestBrowser("https://github.com/WittIsHere/Clever-Engine/issues");
-            ImGui::EndMenu();
-        }
-
-		ImGui::EndMenuBar();
-		ImGui::End();
-	}
+        ImGui::End();
+    }
 
     if (showDemoWindow)
         ImGui::ShowDemoWindow(&showDemoWindow);
@@ -161,7 +191,6 @@ update_status ModuleUI::Update(float dt)
     }
 
     //draw windows
-    DrawDockingSpace(activeDockingSpace);
     DrawConsoleSpace(activeConsole);
     DrawConfigurationSpace(activeConfiguration);
 
@@ -203,100 +232,6 @@ bool ModuleUI::CleanUp()
     ImGui::DestroyContext();
 
 	return true;
-}
-
-void ModuleUI::DrawDockingSpace(bool active)
-{
-    static ImGuiDockNodeFlags dockspace_flags = ImGuiDockNodeFlags_None;
-
-    // We are using the ImGuiWindowFlags_NoDocking flag to make the parent window not dockable into,
-    // because it would be confusing to have two docking targets within each others.
-    ImGuiWindowFlags window_flags = ImGuiWindowFlags_MenuBar | ImGuiWindowFlags_NoDocking;
-    if (WIN_FULLSCREEN)
-    {
-        ImGuiViewport* viewport = ImGui::GetMainViewport();
-        ImGui::SetNextWindowPos(viewport->GetWorkPos());
-        ImGui::SetNextWindowSize(viewport->GetWorkSize());
-        ImGui::SetNextWindowViewport(viewport->ID);
-        ImGui::PushStyleVar(ImGuiStyleVar_WindowRounding, 0.0f);
-        ImGui::PushStyleVar(ImGuiStyleVar_WindowBorderSize, 0.0f);
-        window_flags |= ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove;
-        window_flags |= ImGuiWindowFlags_NoBringToFrontOnFocus | ImGuiWindowFlags_NoNavFocus;
-    }
-    else
-    {
-        dockspace_flags &= ~ImGuiDockNodeFlags_PassthruCentralNode;
-    }
-
-    // When using ImGuiDockNodeFlags_PassthruCentralNode, DockSpace() will render our background
-    // and handle the pass-thru hole, so we ask Begin() to not render a background.
-    if (dockspace_flags & ImGuiDockNodeFlags_PassthruCentralNode)
-        window_flags |= ImGuiWindowFlags_NoBackground;
-
-    // Important: note that we proceed even if Begin() returns false (aka window is collapsed).
-    // This is because we want to keep our DockSpace() active. If a DockSpace() is inactive,
-    // all active windows docked into it will lose their parent and become undocked.
-    // We cannot preserve the docking relationship between an active window and an inactive docking, otherwise
-    // any change of dockspace/settings would lead to windows being stuck in limbo and never being visible.
-
-    if (!WIN_PADDING)
-        ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
-
-    ImGui::Begin("DockSpace Demo", &active, window_flags);
-
-    if (!WIN_PADDING)
-        ImGui::PopStyleVar();
-
-    if (WIN_FULLSCREEN)
-        ImGui::PopStyleVar(2);
-
-    ImGuiIO& io = ImGui::GetIO();
-    if (io.ConfigFlags & ImGuiConfigFlags_DockingEnable)
-    {
-        ImGuiID dockspace_id = ImGui::GetID("MyDockSpace");
-        ImGui::DockSpace(dockspace_id, ImVec2(0.0f, 0.0f), dockspace_flags);
-    }
-    else
-    {
-        ShowDockingDisabledMessage();
-    }
-
-    if (ImGui::BeginMenuBar())
-    {
-        if (ImGui::BeginMenu("Options"))
-        {
-            // Disabling fullscreen would allow the window to be moved to the front of other windows,
-            // which we can't undo at the moment without finer window depth/z control.
-            ImGui::MenuItem("Fullscreen", NULL, WIN_FULLSCREEN);
-            ImGui::MenuItem("Padding", NULL, WIN_PADDING);
-            ImGui::Separator();
-
-            if (ImGui::MenuItem("Flag: NoSplit", "", (dockspace_flags & ImGuiDockNodeFlags_NoSplit) != 0)) { dockspace_flags ^= ImGuiDockNodeFlags_NoSplit; }
-            if (ImGui::MenuItem("Flag: NoResize", "", (dockspace_flags & ImGuiDockNodeFlags_NoResize) != 0)) { dockspace_flags ^= ImGuiDockNodeFlags_NoResize; }
-            if (ImGui::MenuItem("Flag: NoDockingInCentralNode", "", (dockspace_flags & ImGuiDockNodeFlags_NoDockingInCentralNode) != 0)) { dockspace_flags ^= ImGuiDockNodeFlags_NoDockingInCentralNode; }
-            if (ImGui::MenuItem("Flag: AutoHideTabBar", "", (dockspace_flags & ImGuiDockNodeFlags_AutoHideTabBar) != 0)) { dockspace_flags ^= ImGuiDockNodeFlags_AutoHideTabBar; }
-            if (ImGui::MenuItem("Flag: PassthruCentralNode", "", (dockspace_flags & ImGuiDockNodeFlags_PassthruCentralNode) != 0, WIN_FULLSCREEN)) { dockspace_flags ^= ImGuiDockNodeFlags_PassthruCentralNode; }
-            ImGui::Separator();
-
-            if (ImGui::MenuItem("Close", NULL, false, active != NULL))
-                active = false;
-            ImGui::EndMenu();
-        }
-        /* HelpMarker(
-             "When docking is enabled, you can ALWAYS dock MOST window into another! Try it now!" "\n\n"
-             " > if io.ConfigDockingWithShift==false (default):" "\n"
-             "   drag windows from title bar to dock" "\n"
-             " > if io.ConfigDockingWithShift==true:" "\n"
-             "   drag windows from anywhere and hold Shift to dock" "\n\n"
-             "This demo app has nothing to do with it!" "\n\n"
-             "This demo app only demonstrate the use of ImGui::DockSpace() which allows you to manually create a docking node _within_ another window. This is useful so you can decorate your main application window (e.g. with a menu bar)." "\n\n"
-             "ImGui::DockSpace() comes with one hard constraint: it needs to be submitted _before_ any window which may be docked into it. Therefore, if you use a dock spot as the central point of your application, you'll probably want it to be part of the very first window you are submitting to imgui every frame." "\n\n"
-             "(NB: because of this constraint, the implicit \"Debug\" window can not be docked into an explicit DockSpace() node, because that window is submitted as part of the NewFrame() call. An easy workaround is that you can create your own implicit \"Debug##2\" window after calling DockSpace() and leave it in the window stack for anyone to use.)"
-         );*/
-
-        ImGui::EndMenuBar();
-        ImGui::End();
-    }
 }
 
 void ModuleUI::DrawConsoleSpace(bool active)
@@ -502,7 +437,7 @@ void ModuleUI::ShowDockingDisabledMessage()
     ImGui::Text("ERROR: Docking is not enabled! See Demo > Configuration.");
     ImGui::Text("Set io.ConfigFlags |= ImGuiConfigFlags_DockingEnable in your code, or ");
     ImGui::SameLine(0.0f, 0.0f);
-    if (ImGui::SmallButton("click here"))
+    if (ImGui::SmallButton("click here")) 
         io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
 }
 
